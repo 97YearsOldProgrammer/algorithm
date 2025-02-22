35e69e518a501e9d37da0c9ad2c3caa995433d18
97YearsOldProgrammer <dsbchen@ucdavis.edu>
2025-02-22 01:39:56 -0800

    up

--- apc/HMM_isoform.c
+++ apc/HMM_isoform.c
@@ -23,9 +23,7 @@
 typedef struct{
     int N;
     int M;
-    int *pA;
-    int *pB;
-    int *palpha;    
+    int numerical_seq;
 } Hidden_markov_model;
 
 // note if we are not making everything into the log space or whatever turns them into the int
@@ -52,11 +50,6 @@ void set_parameter(Hidden_markov_model *hmm, const int N, const int M)
 
 int A[4][4];
 
-for i in range()
-
-// outer layer: given state X as base pair
-// inner layer: number of hidden state associated with
-
 int B[4][N];
 
 // setting up the alpha for forward-backward algorithm
@@ -75,22 +68,48 @@ int alpha[T][N][M];
 // outer layer: given base pair
 // inner layer: the probability of next base pair be
 
-void sequence_transversion(const *seq)
-{
-    for( i = 0; i < strlen(seq); i++)
-    {
-        
-    }
+int main(void){
+
+    free(hmm.numerical_seq);
 }
+
+// turns original sequence into int 
+// A == 1 , C == 2, G == 3, T == 4
+
+void numerical_transcription(Hidden_markov_model *hmm, const char *seq)
+{
+    
+    size_t len = strlen(seq);
+    hmm->numerical_seq = malloc ( len * sizeof(int));
+
+    for( i = 0; i < len; i++)
+    {
+        if      (seq[i] == 'A'){
+            hmm.numerical_seq[i] = 1
+        }
+        else if (seq[i] == 'C'){
+            hmm.numerical_seq[i] = 2
+        }
+        else if (seq[i] == 'G'){
+            hmm.numerical_seq[i] = 3
+        }
+        else if (seq[i] == 'T'){
+            hmm.numerical_seq[i] = 4
+        }
+    }
+
+}
+
 void transition_matrix(Lamba *l)
 {
     // current transition between each observed base pair is set to 0.25
     // edit directly if we wanna change soemthing 
     // or later for automatic update
 
-    l.A = double A[4][4] = {
+    l->A = double A[4][4] = {
 
     //    A     C     G     T
+
         {0.25, 0.25, 0.25, 0.25}, // A
         {0.25, 0.25, 0.25, 0.25}, // C
         {0.25, 0.25, 0.25, 0.25}, // G
@@ -98,12 +117,52 @@ void transition_matrix(Lamba *l)
 
     }
 
-    
-
-
-
 }
-void forward_algorithm(Hidden_markov_model *HMM, int *alpha, const int *A, const *B, int depth)
+
+void starting_matrix(Lamba *l)
+{
+    l->pi = double pi[4] = {
+
+    //    A     C     G     T
+
+        0.25, 0.25, 0.25, 0.25
+
+    }
+}
+
+void hidden_state_matrix(Lamba *l)
+{
+    // outer layer: given state X as base pair
+    // inner layer: number of hidden state associated with
+    // i assume there is 3 hidden state rn
+
+    l->B = double B[4][3] = {
+    
+    //     ds    as    etc
+        { 0.33, 0.33, 0.33 }, // A
+        { 0.33, 0.33, 0.33 }, // C
+        { 0.33, 0.33, 0.33 }, // G
+        { 0.33, 0.33, 0.33 }  // T
+
+    }
+}
+
+// parameter; given a hidden state we want to find probability on
+// which is const int *hs; pointer of that
+
+// this is calculating first 
+void basis_of_forward_algorithm(Lamba *l)
+{
+    // alpha(current observed state) = P(observed state) * P(hidden state | observed state)
+    // data structure [current position][observed state][hidden state]
+
+    // this part require dynamic allocation
+    // since we cannot tell how much position we need unless we are given a sequence 
+    
+    l->alpha[0][]
+}
+
+void forward_algorithm(Hidden_markov_model *HMM, Lamba *l, int *hs, int *alpha, const int *A, const *B, int depth)
 {
 
 }
